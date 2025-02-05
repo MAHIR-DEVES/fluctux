4a3536d1e68afaf55fc2a060a3a286fedf01a495
gitmahin <141026311+gitmahin@users.noreply.github.com>
2025-02-05 09:25:33 +0600

    updated

--- src/components/core/docs/search/no-results.tsx
+++ src/components/core/docs/search/no-results.tsx
@@ -1,7 +1,5 @@
 "use client"
-
 import FxButton from "@/components/ui/fxbutton";
-import FxFavIcon from "@/components/ui/fxfav";
 import { CodeIcon } from "@/components/ui/icons/code-icon";
 import { RightArrowIcon } from "@/components/ui/icons/right-arrow-icon";
 import { SadIcon } from "@/components/ui/icons/sad-icon";
@@ -22,23 +20,7 @@ export default function NoResults() {
             </div>
 
             <div className="max-w-[500px] w-full mt-10">
-                <Link href={""}>
-                    <FxButton variant="secondary" radius="primary" className="w-full p-3 fx-flex-tl gap-2 relative">
-                        <div className="w-[40px] h-[40px] border fx-border-color fx-flex-center flex-shrink-0 rounded-[5px]">
-                            <FxFavIcon variant="default" size="sm" />
-                        </div>
-                        <div className="text-left">
-
-                            <h3 className="font-medium text-[18px]  ">
-                                What is Fluctux
-                            </h3>
-                            <p className="text-[14px] fx-label-color mt-2 two-line-ellipsis">Fluctux is an advanced open source work and project management platform where users can join both public & private organizations, collaborate in teams, make friends, and share their daily work—all in one place.</p>
-                        </div>
-                        <div className="w-[20px] flex-shrink-0">
-                            <RightArrowIcon className="absolute top-[50%] translate-y-[-50%]"/>
-                        </div>
-                    </FxButton>
-                </Link>
+            {/* Fluctux is an advanced open source work and project management platform where users can join both public & private organizations, collaborate in teams, make friends, and share their daily work—all in one place. */}
                 <Link href={""}>
                     <FxButton variant="secondary" radius="primary" className="w-full p-3 fx-flex-between-ic gap-3 mt-3">
                         <div className="fx-flex-cl gap-2">

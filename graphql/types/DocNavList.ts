5a252ca4bf9927c4513cfae0e382d55d926e60fb
gitmahin <141026311+gitmahin@users.noreply.github.com>
2025-02-05 11:15:37 +0600

    Update DocNavList.ts

--- graphql/types/DocNavList.ts
+++ graphql/types/DocNavList.ts
@@ -42,16 +42,16 @@ export const DocNavListQuery = extendType({
       args: {
         docType: stringArg(),
       },
-      /**
-       * Resolves the document navigation list based on the provided document type.
-       * If no document type is provided, it defaults to "user".
-       *
-       * Unused parameter, typically represents the root object in a resolver.
-       * args - An object containing the arguments for the resolver.
-       * args.docType - An optional string representing the type of document to resolve.
-       *                If null or undefined, defaults to "user".
-      */
       async resolve(_, args: { docType?: string | null }) {
+        /**
+         * Resolves the document navigation list based on the provided document type.
+         * If no document type is provided, it defaults to "user".
+         *
+         * Unused parameter, typically represents the root object in a resolver.
+         * args - An object containing the arguments for the resolver.
+         * args.docType - An optional string representing the type of document to resolve.
+         *                If null or undefined, defaults to "user".
+         */
         const docType = args.docType ?? "user";
         return await docNavListResolver(docType);
       },

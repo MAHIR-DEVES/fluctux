375d3412614a7f3bc4b3dce67bd61483be9385b6
gitmahin <141026311+gitmahin@users.noreply.github.com>
2025-02-06 15:52:07 +0600

    Update no-results.tsx

--- src/components/core/docs/search/no-results.tsx
+++ src/components/core/docs/search/no-results.tsx
@@ -18,42 +18,42 @@ export default function NoResults() {
                 </p>
             </div>
 
-<div className="max-w-[500px] w-full mt-10">
+            <div className="max-w-[500px] w-full mt-10">
 
 
-            <p className="text-[14px] fx-sec-label-color font-medium">Navigate to:</p>
-            <ul className=" w-full mt-1 border fx-border-color overflow-hidden  fx-rounded">
-            {/* Fluctux is an advanced open source work and project management platform where users can join both public & private organizations, collaborate in teams, make friends, and share their daily work—all in one place. */}
-                <Link href={""}>
-                    <li className="w-full p-3 fx-flex-between-ic gap-3 border-b fx-border-color fx-label-color hover:text-[var(--foreground)]  fx-secondary-hover-bg">
-                        <div className="fx-flex-cl gap-2">
-                            <UserIcon />
-                            <p>
-                                Quickstart for Users
+                <p className="text-[14px] fx-sec-label-color font-medium">Navigate to:</p>
+                <ul className=" w-full mt-1 border fx-border-color overflow-hidden  fx-rounded">
+                    {/* Fluctux is an advanced open source work and project management platform where users can join both public & private organizations, collaborate in teams, make friends, and share their daily work—all in one place. */}
+                    <Link href={""}>
+                        <li className="w-full p-3 fx-flex-between-ic gap-3 border-b fx-border-color fx-label-color hover:text-[var(--foreground)]  fx-secondary-hover-bg">
+                            <div className="fx-flex-cl gap-2">
+                                <UserIcon />
+                                <p>
+                                    Quickstart for Users
 
-                            </p>
-                        </div>
-                        <div className="w-[20px] flex-shrink-0">
-                            <RightArrowIcon/>
-                        </div>
-                    </li>
-                </Link>
-                <Link href={""}>
-                    <li className="w-full p-3 fx-flex-between-ic gap-3 fx-label-color hover:text-[var(--foreground)]  fx-secondary-hover-bg">
-                        <div className="fx-flex-cl gap-2">
+                                </p>
+                            </div>
+                            <div className="w-[20px] flex-shrink-0">
+                                <RightArrowIcon />
+                            </div>
+                        </li>
+                    </Link>
+                    <Link href={""}>
+                        <li className="w-full p-3 fx-flex-between-ic gap-3 fx-label-color hover:text-[var(--foreground)]  fx-secondary-hover-bg">
+                            <div className="fx-flex-cl gap-2">
 
-                            <CodeIcon />
-                            <p>
-                                Quickstart for Developers
+                                <CodeIcon />
+                                <p>
+                                    Quickstart for Developers
 
-                            </p>
-                        </div>
-                        <div className="w-[20px] flex-shrink-0">
-                            <RightArrowIcon/>
-                        </div>
-                    </li>
-                </Link>
-            </ul>
+                                </p>
+                            </div>
+                            <div className="w-[20px] flex-shrink-0">
+                                <RightArrowIcon />
+                            </div>
+                        </li>
+                    </Link>
+                </ul>
             </div>
         </div>
     );

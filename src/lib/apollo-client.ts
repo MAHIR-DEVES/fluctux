ab7fc1956e8c0b30ae6c4873483d2b83c493aea0
gitmahin <141026311+gitmahin@users.noreply.github.com>
2025-02-06 15:56:06 +0600

    updated

--- src/lib/apollo-client.ts
+++ src/lib/apollo-client.ts
@@ -2,6 +2,6 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const apolloClient = new ApolloClient({
   // change it to actual base url
-  uri: "http://localhost:3000/api/graphql",
+  uri: process.env.NEXT_PUBLIC_GRAPHQL_API,
   cache: new InMemoryCache(),
 });

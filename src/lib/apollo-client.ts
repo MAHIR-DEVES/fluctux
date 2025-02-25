4eae33639101bac7d37e32479d30663d951ab010
gitmahin <141026311+gitmahin@users.noreply.github.com>
2025-02-25 16:46:00 +0600

    Update apollo-client.ts

--- src/lib/apollo-client.ts
+++ src/lib/apollo-client.ts
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const apolloClient = new ApolloClient({
   // change it to actual base url
-  uri: "https://fluctux.vercel.app",
+  uri: "http://localhost:3000/api/graphql",
   cache: new InMemoryCache(),
   
 });

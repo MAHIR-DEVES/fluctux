6625100a836f9455b9893c024e1b8fecd4c29a4b
gitmahin <141026311+gitmahin@users.noreply.github.com>
2025-02-28 19:04:24 +0600

    updated

--- src/components/ui/fxinput.tsx
+++ src/components/ui/fxinput.tsx
@@ -1,4 +1,4 @@
-import {  SizeType } from "@/components/ui/type";
+import { SizeType } from "@/components/ui/type";
 import React from "react";
 import { ROUNDED_VARIANTS } from "./constant";
 
@@ -19,12 +19,11 @@ const inputVariants: { [key in InputVariantType]: string } = {
   outline: "bg-transparent border fx-border-color w-full focus:outline-fxInput"
 };
 
-
 const inputSizes: { [key in SizeType]: string } = {
-    sm: "p-1 pl-2 pr-2",
-    md: "p-2 pl-3 pr-3",
-    lg: "p-3 pl-4 pr-4",
-    xl: "p-4 pl-5 pr-5",
+  sm: "p-1 pl-2 pr-2",
+  md: "p-2 pl-3 pr-3",
+  lg: "p-3 pl-4 pr-4",
+  xl: "p-4 pl-5 pr-5",
 };
 
 export function FxInput({
@@ -35,14 +34,14 @@ export function FxInput({
   label = "Label",
   ...props
 }: FxInputProps) {
+  
   const inputVariant = variant ? inputVariants[variant] : "";
   const inputSize = size ? inputSizes[size] : "";
   const roundedVariant = radius ? ROUNDED_VARIANTS[radius] : ""
 
- 
   return (
     variant === "outline" ? <div className="w-full relative group">
-      <input className={`peer ${inputVariant} ${inputSize} ${roundedVariant} ${className} `} {...props} id={`${label.replace(" ", "-")}`}  />
+      <input className={`peer ${inputVariant} ${inputSize} ${roundedVariant} ${className} `} {...props} id={`${label.replace(" ", "-")}`} />
       <label htmlFor={`${label.replace(" ", "-")}`} className="absolute translate-y-[-50%] fx-label-color peer-focus:text-[var(--primary-color)] left-[15px] fx-primary-bg py-0 px-1 text-[14px] font-medium ">{label}</label>
     </div> : <input className={`${inputVariant} ${inputSize} ${roundedVariant} ${className}`} {...props} />
   );
